07e5c79ebe2d194c46ced048fa0d3414e41917f3
HectorCdn <hcottachaudon@edenschool.fr>
2024-10-14 15:34:11 +0200

    feat

---
+++ Ex1/Celsius.js
@@ -0,0 +1,5 @@
+let convertion = prompt("Quel temps fait-il ?")
+
+convertion = convertion * 9/5 + 32
+
+console.log(`Il fait ${convertion} °F`)
---
+++ Ex2/addition.js
@@ -0,0 +1,10 @@
+let a = parseFloat(prompt("Choisie un premier nombre."));
+let b = parseFloat(prompt("Choisie un deuxième nombre."));
+let c = parseFloat(prompt("Choisie un troixième nombre."));
+
+a = a + b;
+c = a + c;
+
+sommeTroisNombres = c;
+
+console.log(sommeTroisNombres);
