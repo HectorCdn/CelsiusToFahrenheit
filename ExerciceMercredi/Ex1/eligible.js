15db3832768653e1add39a2201ca60edf4cb2db2
HectorCdn <hcottachaudon@edenschool.fr>
2024-10-16 10:15:44 +0200

    feat

--- ExerciceMercredi/Ex1/eligible.js
+++ ExerciceMercredi/Ex1/eligible.js
@@ -1,15 +1,8 @@
 let age = Number(prompt("Quel âge avez-vous ?"));
 let nation = prompt("De quel nationalité êtes-vous ?");
 
-if (age >= 18) {
+if (age >= 18 && nation == "francaise") {
     console.log("Vous êtes éligible")
   } else {
     console.log("Vous n'êtes pas éligible")
   }
-
-
-if (nation = "français") {
-      console.log("Vous êtes éligible")
-}  else {
-      console.log("Vous n'êtes pas éligible")
-}
--- ExerciceMercredi/ex2/age.js
+++ ExerciceMercredi/ex2/age.js
@@ -1,10 +1,9 @@
 let age = parseFloat(prompt("Quel âge avez-vous ?"));
 
-if (age <= 5)
-  console.log("Gratuit")
-else
-  if(age <= 17)
-    console.log("tarif réduit")
-  else
-    if (age >= 18)
-      console.log("Plein tarif")
+if (age <= 5) {
+  console.log("Gratuit");
+} else if(age <= 17) {
+    console.log("tarif réduit");
+} else if (age >= 18) {
+      console.log("Plein tarif");
+}
