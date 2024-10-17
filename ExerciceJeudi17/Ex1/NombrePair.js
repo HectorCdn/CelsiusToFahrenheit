6efeb3ac18ee7a032429a2b9e2cab6c33394c05f
HectorCdn <hcottachaudon@edenschool.fr>
2024-10-17 14:19:19 +0200

    feat

---
+++ ExerciceJeudi17/Ex1/NombrePair.js
@@ -0,0 +1,18 @@
+let n = 0;
+
+while (n <= 1000) {
+  n = n + 1
+  if (n % 2 === 0) {
+    console.log(n);
+  }
+}
+
+//Et pour les nombres ronds
+
+//let n = 0;
+//while (n <= 1000) {
+//  n = n + 1
+//  if (n % 10 === 0) {
+//    console.log(n);
+//  }
+//}
